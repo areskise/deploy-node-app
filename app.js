8e249e696959cb53e112ddb6125320ede3002774
Ares Kise <areskise@gmail.com>
2022-12-18 15:52:49 +0700

    fix mongoUrl

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const errorController = require('./controllers/error');
 const User = require('./models/user');
 
 const MONGODB_URI =
-  'mongodb+srv://areskise:24110399@cluster0.vsce8sb.mongodb.net/shop';
+  'mongodb+srv://areskise:24110399@cluster0.vsce8sb.mongodb.net/shop?retryWrites=true&w=majority';
 
 const app = express();
 const store = new MongoDBStore({
